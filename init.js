6bc84da0e53adcf477d6689812396d954f83d0af
Gregory Robertson <robertsongregorya@gmail.com>
2021-12-01 17:49:50 -0500

    Change articleid obtaining process, & Add getcachereadarticle

--- init.js
+++ init.js
@@ -16,8 +16,8 @@ async function main() {
     await aquery(`USE ${PICONFIG.DB}`);
     await aquery(`CREATE TABLE Users (Name nvarchar(30) PRIMARY KEY, Hash char(60), Token BINARY(64))`);
     // basically this table only exists to state that each article can only happen once
-    await aquery(`CREATE TABLE Articles (Id smallint unsigned PRIMARY KEY);`);
-    await aquery(`CREATE TABLE UserReadArticles (UserName nvarchar(30), ArticleId smallint unsigned,` + 
+    await aquery(`CREATE TABLE Articles (Id int unsigned PRIMARY KEY);`);
+    await aquery(`CREATE TABLE UserReadArticles (UserName nvarchar(30), ArticleId int unsigned,` + 
         `FOREIGN KEY (UserName) REFERENCES Users(Name),` +
         `FOREIGN KEY (ArticleId) REFERENCES Articles(Id)` +
     `);`);
--- src/article-helpers.js
+++ src/article-helpers.js
@@ -4,8 +4,14 @@ export function articleFromDate(date) {
     return `https://nhkeasier.com/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
 }
 
-export function articleIdFromDate(date) {
-    return ((date.getFullYear() - 2000) * 365) + (date.getMonth() * 30) + (date.getDate());
+export function articleIdFromDateAndArtNumber(date, artnumber) {
+    // heavily bodged together, each day can only have 8 articles (should be enough)
+    if (artnumber > 8) artnumber = 8;
+    if (artnumber < 0) artnumber = 0;
+    let articleId = ((date.getFullYear() - 2000) * 12 * 31 * 9) + (date.getMonth() * 31 * 9) + (date.getDate() * 9) + artnumber;
+    if (articleId < 0) articleId = 0;
+    if (articleId > 65535) articleId = 65535;
+    return articleId;
 }
 
 export async function createArticleInDB(articleId) {
