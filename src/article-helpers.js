5bb27e5359fefcd3afc7b6ed07b7eb4555fb921e
Gregory Robertson <robertsongregorya@gmail.com>
2021-12-01 09:18:42 -0500

    Finish createArticleInDB()

--- src/article-helpers.js
+++ src/article-helpers.js
@@ -8,8 +8,9 @@ export function articleIdFromDate(date) {
     return ((date.getFullYear() - 2000) * 365) + (date.getMonth() * 30) + (date.getDate());
 }
 
-export function createArticleInDB(date) {
+export async function createArticleInDB(articleId) {
     const aquery = promisify(CONFIG.CONN.query).bind(CONFIG.CONN);
     await aquery(`USE ${CONFIG.DB}`);
 
+    await aquery(`INSERT INTO Articles VALUES (?)`, articleId);
 }
