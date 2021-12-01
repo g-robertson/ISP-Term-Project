e4f45fbfd8902172b71563721e90e8670ed11a08
Gregory Robertson <robertsongregorya@gmail.com>
2021-12-01 04:34:13 -0500

    Make calendar point to nhkpage api

--- gatsby/src/components/default_layout.js
+++ gatsby/src/components/default_layout.js
@@ -98,7 +98,7 @@ const Layout = ({ pageTitle, children }) => {
 						onChange={onChange}
 						value={value}
 						maxDate={new Date()}
-						onClickDay={(value) => {window.location.assign(`${PICONFIG.HTTP.host}:${PICONFIG.HTTP.port}/` + (value.toISOString()))}}
+						onClickDay={(date) => {window.location.assign(`/api/nhkpage?date=${date.valueOf()}&artnumber=0`)}}
 					/>
 				</div>
 				<div id="dropLinks" className={drop} style={{display:'none'}}>
