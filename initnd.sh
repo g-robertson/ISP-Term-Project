c91b2664b095c80b12ca1f67927cdeccc8f4efd5
Gregory Robertson <robertsongregorya@gmail.com>
2021-12-04 08:03:27 -0500

    Add no database init

---
+++ initnd.sh
@@ -0,0 +1,15 @@
+npm install
+cd gatsby
+npm install
+cd ..
+
+cp pre-init-config.js gatsby/pre-init-config.js
+cp dbuser.js gatsby/dbuser.js
+cp dbpassword.js gatsby/dbpassword.js
+
+rm -r public
+rm -r gatsby/public
+cd gatsby
+npm run build
+cd ..
+mv gatsby/public public
