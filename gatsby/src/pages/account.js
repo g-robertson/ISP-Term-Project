485a9494219dab2dddeec488b4f4767f41d31283
Seth Milojevic <seth.milojevic@gmail.com>
2021-12-04 10:15:00 -0500

    Display read articles to logged in users.

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -3,23 +3,7 @@ import Layout from '../components/account_layout'
 
 const AccountPage = () => {
 	return (
-		<Layout
-		user="Seth"
-		kanjiEntries={ 
-			[
-				{kanji:"水",count:"45",articles:[
-					{checkedDate:"2021-11-30",articleLink:"/2021-11-30/3"},
-					{checkedDate:"2021-11-30",articleLink:"/2021-11-30/1"},
-					{checkedDate:"2021-11-30",articleLink:"/2021-11-30/5"},
-					{checkedDate:"2021-11-30",articleLink:"/2021-11-30/5"}
-				]},
-				{kanji:"川",count:"24",articles:[
-					{checkedDate:"2021-11-30",articleLink:"/2021-11-30/3"},
-					{checkedDate:"2021-11-30",articleLink:"/2021-11-30/1"},
-					{checkedDate:"2021-11-30",articleLink:"/2021-11-30/5"}
-				]},
-			]
-		}>
+		<Layout>
 		</Layout>
 	)
 }
