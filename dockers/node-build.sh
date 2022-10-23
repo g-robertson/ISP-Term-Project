c0be2bb6758e6f1eac53d07316ac96de8050862a
Gregory Robertson <robertson.gregory.a@gmail.com>
2022-10-23 01:33:27 -0400

    Force node build to require postgres rebuild and run

--- dockers/node-build.sh
+++ dockers/node-build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+./dockers/postgres-build.sh
+./dockers/postgres-run.sh headless
+
 docker build -t jpez-node-init -f dockers/Dockerfile.node-init .
 docker rm -f jpez-node-init
 docker create --name jpez-node-init jpez-node-init
