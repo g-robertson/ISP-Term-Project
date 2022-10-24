baec5d025e8697000604b136721320e4eabe5f8b
Seth Milojevic <sm464@uakron.edu>
2022-10-24 01:11:10 -0400

    Remove gatsby-browser

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-export const onClientEntry = () => {
-    window.onload = () => { document.getElementById("curDate").getElementsByTagName("p")[0].textContent = window.location.pathname.substring(1) }
-}
