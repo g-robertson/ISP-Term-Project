94782ca7bc0b63cc9317f78f4c11fd8e4bf69075
Gregory Robertson <robertson.gregory.a@gmail.com>
2022-11-26 07:46:58 -0500

    Add search context, make bold where search query matches

--- src/pages/search.js
+++ src/pages/search.js
@@ -26,6 +26,35 @@ const SearchResult = ({ article }) => (
                 }
             })()
         }</a>
+        <p>{
+            (() => {
+                // if there's no occurrence of keyword in article, just send back the first 30 non-ascii characters
+                if (article.first_occurrence_in_article === undefined) {
+                    // this regex solution sometimes cuts off really early if a tag is immediately following because it cannot parse it 
+                    // TODO: proper solution would involve some form of skipping over tags properly, or storing a non-tag set of data
+                    // hack solution, only works assuming that there aren't 20 character long numeric expressions in a tag
+                    // assumes the text that we want isn't in ascii (it isn't for our very specific use case)
+                    let nonASCII = article.content.match(/([^\x00-\xFF]|[0-9]){20,}/)[0];
+                    console.log(article, nonASCII);
+                    return nonASCII.substring(0, 30);
+                // if there is occurrence of keyword in article, send back 30 characters starting at the occurrence, then bold all occurrences
+                } else {
+                    let content = article.content.substring(article.first_occurrence_in_article, article.first_occurrence_in_article + 30);
+                    content = content.match(/([^\x00-\xFF]|[0-9])+/)[0];
+                    let query = new URLSearchParams(window.location.search).get("q");
+                    let contentParts = content.split(query);
+                    for (let i = 0; i < contentParts.length - 1; ++i) {
+                        contentParts[i] = (
+                            <>{contentParts[i]}<b>{query}</b></>
+                        );
+                    }
+                    contentParts[contentParts.length - 1] = (
+                        <>{contentParts[contentParts.length - 1]}</>
+                    )
+                    return contentParts;
+                }
+            })()
+        }</p>
     </li>
 );
 
